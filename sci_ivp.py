c0b57feb22e45e77dec3bdc4216b6f4ad495be5f
DonXI <s6201012620082@email.kmutnb.ac.th>
2023-03-31 23:26:57 +0700

    scipy

---
+++ sci_ivp.py
@@ -0,0 +1,35 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+def func1(t,y):
+    dydt = -0.3 * y
+    return dydt
+
+def func2(t,y):
+    dydt = 3 * np.exp(-t)
+    return dydt
+
+def func3(t,y):
+    dydt = 0.5 * (t-1) * y
+    return dydt
+
+# equation 1) y' = -0.3*y, y0=1
+# equation 2) y' = 3*e^(-t), y0 = 0
+# equation 3) y' = 0.5*(t-1)y, y0 = 1
+
+def plot_graph(sol):
+    plt.plot(sol.t, sol.y[0])
+    plt.xlabel("time")
+    plt.ylabel('y(t)')
+    plt.show()
+    plt.title("y' = -0.3*y")
+    #plt.savefig("xxx")
+
+t_span = [0,20]
+y0 = [1]
+sol = solve_ivp(func1, t_span, y0, t_eval=np.arange(0,20,0.1))
+# result
+print(len(sol.y[0]), sol.y)
+print(len(sol.t), sol.t)
+plot_graph(sol)
---
+++ sci_odeint.py
@@ -0,0 +1,30 @@
+import numpy as np 
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+def func1(y,t):
+    k = 0.3
+    dydt = -k * y
+    return dydt
+
+def func2(y,t):
+    k = 3
+    dydt = k * np.exp(-t)
+    return dydt
+
+def func3(y,t):
+    k = 0.5
+    dydt = k * (t-1) * y
+    return dydt
+
+# result 
+y0 = 1
+t = np.linspace(0,20,200)
+print(len(t), t)
+y = odeint(func1, y0, t)
+print(len(y), y)
+
+plt.plot(t,y)
+plt.xlabel("time")
+plt.ylabel('y(t)')
+plt.show()
